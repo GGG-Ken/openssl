78eba4b6afc09069d2d42d150f263caa7c05c909
zhushiquan <zhushiquan@seevision.cn>
2025-04-23 11:42:26 +0800

    Wed Apr 23 11:42:26 AM CST 2025

--- kenbuild.sh
+++ kenbuild.sh
@@ -1,8 +1,12 @@
+#csdn 如下：
+#https://blog.csdn.net/wkd_007/article/details/138673265?ops_request_misc=%257B%2522request%255Fid%2522%253A%2522f5292c28099b19a207a6c64f7c3ca119%2522%252C%2522scm%2522%253A%252220140713.130102334.pc%255Fall.%2522%257D&request_id=f5292c28099b19a207a6c64f7c3ca119&biz_id=0&utm_medium=distribute.pc_search_result.none-task-blog-2~all~first_rank_ecpm_v1~hot_rank-4-138673265-null-null.142^v102^control&utm_term=Mosquitto%20&spm=1018.2226.3001.4187
+
+
 #arm64
-# ./config no-asm shared no-async --prefix=`pwd`/ssl_result  --cross-compile-prefix=/home/quan/share/zc0203_skdl0401p/tools/gcc-10.2.1-20210303-sigmastar-glibc-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+# ./config no-asm shared no-async --prefix=`pwd`/ken_result  --cross-compile-prefix=/home/quan/share/zc0203_skdl0401p/tools/gcc-10.2.1-20210303-sigmastar-glibc-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 
 #arm32
-./config no-asm shared no-async --prefix=`pwd`/ssl_result  --cross-compile-prefix=/home/quan/share/sktc0405/tools/gcc-11.1.0-20210608-sigmastar-glibc-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
+./config no-asm shared no-async --prefix=`pwd`/ken_result  --cross-compile-prefix=/home/quan/share/sktc0405/tools/gcc-11.1.0-20210608-sigmastar-glibc-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
 
 sed -i 's/-m64//' Makefile
 make && make install
---
+++ pushgithub.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 提示用户输入提交信息
+read -p "请输入提交信息: " commit_message
+current_branch=$(git branch --show-current)
+echo "current branch= $current_branch"
+# 执行 Git 命令
+#git add -u
+git add .
+git commit -m "$commit_message $(date)"
+git push origin $current_branch
+
+echo "Git 命令已执行完毕。"
